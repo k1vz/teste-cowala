f68fc3d9a1e33e006aa372663a3f28760d9fcec1
k1vz <mrnerd777@gmail.com>
2022-01-23 17:48:43 -0300

    added a try-catch when calling external api

--- src/utils/callCurrencyAPI.ts
+++ src/utils/callCurrencyAPI.ts
@@ -2,9 +2,13 @@ import axios from 'axios'
 import { ICurrencyAPI } from '../interfaces/ICurrencyConversion'
 
 export async function callCurrencyAPI(code: string): Promise<ICurrencyAPI> {
-	const response = await axios.get(`https://economia.awesomeapi.com.br/last/${code}`)
+	try {
+		const response = await axios.get(`https://economia.awesomeapi.com.br/last/${code}`)
 
-	const data = response.data
+		const data = response.data
 
-	return data
+		return data
+	} catch(err) {
+		throw new Error(err)
+	}
 }
